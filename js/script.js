94b1cc9bc862fff3708d904057e0ccd8889ec655
BobCHub <ender1117@yahoo.com>
2018-02-12 12:49:05 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function submitAnswers() {
 	}
 
 	// Set Correct Answers
-	var answers = ["b", "a", "d", "b", "d"];
+	var answers = ["a", "c", "a", "b", "c"];
 
 	// Check Answers
 	for ( i = 1; i <= total; i++) {
